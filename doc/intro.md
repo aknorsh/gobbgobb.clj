b0bbc58fd4a29c7ff658488549d6f09a5f888d79
AKINORI Ishii <aknorsh@gmail.com>
2019-12-30 23:05:02 +0900

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gobbgobb
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/gobbgobb/core_test.clj
@@ -0,0 +1,7 @@
+(ns gobbgobb.core-test
+  (:require [clojure.test :refer :all]
+            [gobbgobb.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
